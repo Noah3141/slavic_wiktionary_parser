dd0df2ee47c081c16c28a836a277a95f146d70b8
Noah Steckley <Noah3141@gmail.com>
2024-07-28 22:24:55 -0400

    Excellent! We have basic counts

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use wiktionary_parser::models::language::Language;
+use std::{fs::File, io::BufReader};
+
+use wiktionary_parser::models::{language::Language, wiktionary_macro::{
+    russian::*, WiktionaryMacro 
+}};
 
 mod processes;
 mod traits;
@@ -35,12 +39,52 @@ fn main() {
     //     true,
     //     Language::Russian
     // ).expect("success");
-    processes::filtered_to_json(
-        "C:/Users/Noah3/Code/slavic_wiktionary_parser/data/filtered_wiki_dump/ukrainian.txt",
-        "C:/Users/Noah3/Code/slavic_wiktionary_parser/data/parsed/ukrainian.json",
-        true,
-        Language::Ukrainian
-    ).expect("success");
+    // processes::filtered_to_json(
+    //     "C:/Users/Noah3/Code/slavic_wiktionary_parser/data/filtered_wiki_dump/ukrainian.txt",
+    //     "C:/Users/Noah3/Code/slavic_wiktionary_parser/data/parsed/ukrainian.json",
+    //     true,
+    //     Language::Ukrainian
+    // ).expect("success");
 
+    println!("Reading file...");
+    let reader = BufReader::with_capacity(1024*1024*250, File::open("C:/Users/Noah3/Code/slavic_wiktionary_parser/data/parsed/belarusian.json").unwrap());
+    let wiki_macros: Vec<WiktionaryMacro> = serde_json::from_reader(reader).unwrap();
+    println!("Finished!");
+
+    let noun_count = wiki_macros.iter()
+        .filter_map(|m| match m {
+            WiktionaryMacro::BeNoun(m) => Some(m),
+            _ => None,
+        })
+        .count();
+    let verb_count: usize = wiki_macros.iter()
+        .filter_map(|m| match m {
+            WiktionaryMacro::BeVerb(m) => Some(m),
+            _ => None,
+        })
+        .count();
+    let adj_count: usize = wiki_macros.iter()
+        .filter_map(|m| match m {
+            WiktionaryMacro::BeAdj(m) => Some(m),
+            _ => None,
+        })
+        .count();
+
+    let inflected_count: usize = wiki_macros.iter()
+        .filter_map(|m| {match m {
+            WiktionaryMacro::InflectionOf(m) => Some(m),
+            _ => None,
+        }})
+        .filter(|infl| {infl.language == Language::Belarusian})
+        .count();
+
+    println!("--Belarusian--");
+    println!("Nouns: {noun_count}");
+    println!("Verbs: {verb_count}");
+    println!("Adjs: {adj_count}");
+    println!("Total: {}", noun_count + verb_count + adj_count );
+    println!("-------------");
+
+    println!("Inflected: {inflected_count}");
 
 }
